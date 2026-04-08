ca3d64daeb8468dfff03e231db91ecac2733037b
Vinicius Pereira <52613474+ViniciusLira1311@users.noreply.github.com>
2026-04-08 17:54:24 -0300

    Add 'Monoalfabética' to available ciphers

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Configuração das cifras disponíveis
-NOMES_DAS_CIFRAS = ["César"]
+NOMES_DAS_CIFRAS = ["César", "Monoalfabética"]
 CIFRAS_COM_CHAVE = ["César"]   # apenas César precisa do campo de deslocamento
 
 # Mapeamento nome do arquivo (dentro da pasta cifras/)
 MAPEAMENTO_MODULOS = {
     "César":  "cesar",
-    
+    "Monoalfabética": "monoalfabetica"
 }
